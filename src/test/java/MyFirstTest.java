1004ab1234e18f94293824f71d6312083e01d653
james-willett <james.willett58@gmail.com>
2022-11-09 07:54:19 +0000

    Adding code for video 2.7

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,4 +1,5 @@
 import config.VideoGameConfig;
+import config.VideoGameEndpoints;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.*;
@@ -14,5 +15,11 @@ public class MyFirstTest extends VideoGameConfig {
         .then()
                 .log().all();
 
+    }
+
+    @Test
+    public void myFirstTestWithEndpoint() {
+        get(VideoGameEndpoints.ALL_VIDEO_GAMES)
+                .then().log().all();
     }
 }
---
+++ src/test/java/config/VideoGameEndpoints.java
@@ -0,0 +1,7 @@
+package config;
+
+public interface VideoGameEndpoints {
+
+    String ALL_VIDEO_GAMES = "/videogame";
+    String SINGLE_VIDEO_GAME = "/videogame/{videoGameId}";
+}
