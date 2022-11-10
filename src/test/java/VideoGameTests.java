973610ad1183d3809c315cf8dcae56a6838d18bc
james-willett <james.willett58@gmail.com>
2022-11-10 07:10:39 +0000

    Adding code for video 3.2

--- src/test/java/VideoGameTests.java
+++ src/test/java/VideoGameTests.java
@@ -13,4 +13,21 @@ public class VideoGameTests extends VideoGameConfig {
                 .get(VideoGameEndpoints.ALL_VIDEO_GAMES)
         .then();
     }
+
+    @Test
+    public void createNewGameByJSON() {
+        String gameBodyJson = "{\n" +
+                "  \"category\": \"Platform\",\n" +
+                "  \"name\": \"Mario\",\n" +
+                "  \"rating\": \"Mature\",\n" +
+                "  \"releaseDate\": \"2022-05-04\",\n" +
+                "  \"reviewScore\": 89\n" +
+                "}";
+
+        given()
+                .body(gameBodyJson)
+        .when()
+                .post(VideoGameEndpoints.ALL_VIDEO_GAMES)
+        .then();
+    }
 }
