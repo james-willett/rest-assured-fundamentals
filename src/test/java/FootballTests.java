51e48d19fc188598e769c7b6485fc3dad8d6ea6f
james-willett <james.willett58@gmail.com>
2022-11-10 07:55:37 +0000

    Adding code for video 3.7

---
+++ src/test/java/FootballTests.java
@@ -0,0 +1,6 @@
+import config.FootballConfig;
+
+public class FootballTests extends FootballConfig {
+
+    
+}
---
+++ src/test/java/config/FootballConfig.java
@@ -0,0 +1,28 @@
+package config;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import org.junit.BeforeClass;
+
+public class FootballConfig {
+
+    @BeforeClass
+    public static void setup() {
+
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .setBaseUri("https://api.football-data.org")
+                .setBasePath("/v4")
+                .addHeader("X-Auth-Token", "8df69ce914ac49e5a64a485ad355ef56")
+                .addHeader("X-Response-Control", "minified")
+                .addFilter(new RequestLoggingFilter())
+                .addFilter(new ResponseLoggingFilter())
+                .build();
+
+        RestAssured.responseSpecification = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .build();
+    }
+}
