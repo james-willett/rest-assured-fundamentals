c96be5fd489bef653b16a65f148c44f989d45e9d
james-willett <james.willett58@gmail.com>
2022-11-10 15:58:39 +0000

    Adding code for video 3.8

--- src/test/java/FootballTests.java
+++ src/test/java/FootballTests.java
@@ -1,6 +1,26 @@
 import config.FootballConfig;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
 
 public class FootballTests extends FootballConfig {
 
-    
+    @Test
+    public void getDetailsOfOneArea() {
+        given()
+                .queryParam("areas", 2076)
+        .when()
+                .get("/areas");
+    }
+
+    @Test
+    public void getDetailsOfMultipleAreas() {
+        String areaIds = "2076,2077,2080";
+
+        given()
+                .urlEncodingEnabled(false)
+                .queryParam("areas", areaIds)
+        .when()
+                .get("/areas");
+    }
 }
