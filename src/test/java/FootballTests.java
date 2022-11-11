f1b2d96b26acd317aa6e2d97ca0d258cbb03270c
james-willett <james.willett58@gmail.com>
2022-11-11 06:56:52 +0000

    Adding code for video 4.3

--- src/test/java/FootballTests.java
+++ src/test/java/FootballTests.java
@@ -64,4 +64,18 @@ public class FootballTests extends FootballConfig {
         String jsonResponseAsString = response.asString();
         System.out.println(jsonResponseAsString);
     }
+
+    @Test
+    public void extractHeaders() {
+        Response response =
+                get("teams/57")
+                        .then()
+                        .extract().response();
+
+        String contentTypeHeader = response.getContentType();
+        System.out.println(contentTypeHeader);
+
+        String apiVersionHeader = response.getHeader("X-API-Version");
+        System.out.println(apiVersionHeader);
+    }
 }
